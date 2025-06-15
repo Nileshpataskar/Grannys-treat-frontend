b553c5d459f833edea261925a0e8903c988fa64d
Nilesh <nileshpataskars@gmail.com>
2025-06-15 16:59:45 +0530

    Updated ContactForm styles for improved aesthetics and added decorative images to Page9 component.

--- src/app/ContactForm.jsx
+++ src/app/ContactForm.jsx
@@ -1,37 +1,37 @@
 const ContactForm = () => {
   return (
-    <div className="bg-[#d2eef9] p-8 rounded-lg max-w-xl mx-auto mt-10">
+    <div className="bg-[#d2eef9] p-8 rounded-lg max-w-3xl mx-auto mt-10">
       <form>
         <div className="mb-4">
           <input
             type="text"
             placeholder="Your Name"
-            className="w-full p-5  border-[6px] border-[#55acee] rounded-xl placeholder:text-[#fff] placeholder:text-xl placeholder:font-bold focus:outline-none focus:ring-1 focus:ring-[#55acee] cursor-text"
+            className="w-full p-5  border-[7px] border-[#55acee] rounded-3xl placeholder:text-[#fff] placeholder:text-2xl placeholder:font-bold focus:outline-none focus:ring-1 focus:ring-[#55acee] cursor-text"
           />
         </div>
         <div className="mb-4">
           <input
             type="email"
             placeholder="Email Address"
-            className="w-full p-5  border-[6px] border-[#55acee] rounded-xl placeholder:text-[#fff] placeholder:text-xl placeholder:font-bold focus:outline-none focus:ring-1 focus:ring-[#55acee]"
+            className="w-full p-5  border-[7px] border-[#55acee] rounded-3xl placeholder:text-[#fff] placeholder:text-2xl placeholder:font-bold focus:outline-none focus:ring-1 focus:ring-[#55acee]"
           />
         </div>
         <div className="mb-4">
           <input
             type="text"
             placeholder="Topic"
-            className="w-full p-5  border-[6px] border-[#55acee] rounded-xl placeholder:text-[#fff] placeholder:text-xl placeholder:font-bold focus:outline-none focus:ring-1 focus:ring-[#55acee]"
+            className="w-full p-5  border-[7px] border-[#55acee] rounded-3xl placeholder:text-[#fff] placeholder:text-2xl placeholder:font-bold focus:outline-none focus:ring-1 focus:ring-[#55acee]"
           />
         </div>
         <div className="mb-4">
           <textarea
             placeholder="Your Message...."
             rows={7}
-            className="w-full p-5  border-[6px] border-[#55acee] rounded-xl placeholder:text-[#fff] placeholder:text-xl placeholder:font-bold focus:outline-none focus:ring-1 focus:ring-[#55acee]"
+            className="w-full p-5  border-[7px] border-[#55acee] rounded-3xl placeholder:text-[#fff] placeholder:text-2xl placeholder:font-bold focus:outline-none focus:ring-1 focus:ring-[#55acee]"
           />
         </div>
         <button type="submit" className=" w-full justify-center flex cursor-pointer ">
-          <img src="/assets/BG/button.svg" alt="send" className="w-44" />
+          <img src="/assets/BG/button.svg" alt="send" className="w-50" />
         </button>
       </form>
     </div>
--- src/components/Page9.jsx
+++ src/components/Page9.jsx
@@ -7,6 +7,23 @@ const Page9 = () => {
         Drop us a message!
       </p>
       <ContactForm />
+
+      <img
+        src="/assets/p3/strawberry-group.png"
+        alt="Decorative strawberry illustration"
+        className="absolute w-[clamp(80px,22vw,285px)] h-auto aspect-square
+        top-[8%] sm:top-[12%] md:top-[18%] lg:top-[30%] 
+        left-[2%] sm:left-[6%] md:left-[10%] lg:-left-[5%]
+         opacity-50 pointer-events-none select-none z-0"
+      />
+      <img
+        src="/assets/p3/melisa-1.png"
+        alt="Decorative strawberry illustration"
+        className="absolute w-[clamp(80px,22vw,285px)] h-auto aspect-square
+        top-[8%] sm:top-[12%] md:top-[18%] lg:top-[30%] 
+        right-[2%] sm:right-[6%] md:right-[10%] lg:-right-[8%]
+         opacity-50 pointer-events-none select-none z-0"
+      />
     </div>
   );
 };
